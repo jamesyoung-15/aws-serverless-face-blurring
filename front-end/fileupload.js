612c1c697de7fd81a478c4b942c6d662ff7796e2
James <jyyoung@jyymail.com>
2024-11-25 14:27:57 -0500

    update front-end

--- front-end/fileupload.js
+++ front-end/fileupload.js
@@ -1,26 +1,37 @@
 
 const uploadElement = document.getElementById("image-upload");
+const modalElement = document.getElementById("modal");
+const blurButton = document.getElementById("blur-button");
+let closeModal = document.getElementById("close-modal");
+const modalMessage = document.getElementById("modal-message");
 
+// upload image
+uploadElement.addEventListener("change", () => {
+    let imageFileName = document.getElementById("image-file-name");
+    if (uploadElement.files.length == 1) {
+        console.log("File selected: ", uploadElement.files[0]);
 
-uploadElement.addEventListener("cancel", () => {
-        console.log("Cancelled.");
-    });
-    uploadElement.addEventListener("change", () => {
-        const imageFileName = document.getElementById("image-file-name");
-        let blurButton = document.getElementById("blur-button");
-        if (uploadElement.files.length == 1) {
-            console.log("File selected: ", uploadElement.files[0]);
-            let fileSize = returnFileSize(uploadElement.files[0].size);
-            console.log("File size: ", fileSize);
-            if (uploadElement.files[0].size > 20*10**6) {
-                console.log("File too large: ", uploadElement.files[0].size);
-                return;
-            }
-            imageFileName.textContent = uploadElement.files[0].name;
-            blurButton.classList.remove("is-hidden");
+        // check if file size is greater than 15MB
+        let fileSize = returnFileSize(uploadElement.files[0].size);
+        // console.log("File size: ", fileSize);
+        if (fileSize > 15*10**6) {
+            console.log("File too large: ", uploadElement.files[0].size);
+            return;
         }
+        
+
+        imageFileName.textContent = uploadElement.files[0].name;
+        blurButton.classList.remove("is-hidden");
+    }
 });
 
+blurButton.addEventListener("click", () => {
+    modalElement.classList.add("is-active");
+});
+
+closeModal.addEventListener("click", () => {
+    modalElement.classList.remove("is-active");
+});
 
 let returnFileSize = (size) => {
     if(size == 0) return "0 Bytes";

e40bc35d146072e96d2d0bbcedd3cd0603dec3ff
James <jyyoung@jyymail.com>
2025-01-22 14:49:13 -0500

    persist user theme in localstorage

--- front-end/src/ThemeToggle.jsx
+++ front-end/src/ThemeToggle.jsx
@@ -3,16 +3,23 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 
 const ThemeToggle = () => {
-  const [isDarkTheme, setIsDarkTheme] = useState(true);
+  const [isDarkTheme, setIsDarkTheme] = useState(() => {
+    // Get the saved theme from localStorage or default to true
+    const savedTheme = localStorage.getItem('theme');
+    return savedTheme === 'light' ? false : true;
+  });
 
+  // toggle theme by adding and removing theme class in HTML attribute
   useEffect(() => {
     const htmlElement = document.querySelector('html');
     if (isDarkTheme) {
       htmlElement.classList.add('theme-dark');
       htmlElement.classList.remove('theme-light');
+      localStorage.setItem('theme', 'dark'); // Save the theme in localStorage
     } else {
       htmlElement.classList.add('theme-light');
       htmlElement.classList.remove('theme-dark');
+      localStorage.setItem('theme', 'light'); // Save the theme in localStorage
     }
   }, [isDarkTheme]);
 
